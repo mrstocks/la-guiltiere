bfc91c717c37c9fa29a135cd097267c04870ccc4
Mrstocks <mrstocks@elevage-fees-de-celestia.dedibox.fr>
2014-02-12 02:12:20 +0100

    Contact us section

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Laguiltiere::Application.routes.draw do
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
+
   root "pages#home"    
   get "home", to: "pages#home", as: "home"
   get "inside", to: "pages#inside", as: "inside"
+  get "contact_us/index"
   
     
   devise_for :users
