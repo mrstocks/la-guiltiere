2897c50d25b8e38b7df8e2092ee2a8efa64f9569
Mrstocks <mrstocks@la-guiltiere.fr>
2014-02-14 21:11:12 +0100

    All the about section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,15 @@ class PagesController < ApplicationController
   def home
     @sliders = Slider.find(:all);
   end
+
+  def about_the_house
+  end
+
+  def about_the_caravan
+  end
+
+  def about_the_garden
+  end
   
   def inside
   end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,30 @@
 Laguiltiere::Application.routes.draw do
   
-  get '/rates(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
+  get '/rates(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
 
   root "pages#home"    
+
+  # About section 
+  get "about_the_house", to: 'pages#about_the_house', as: "about_the_house"
+  get "about_the_gardens", to: 'pages#about_the_garden', as: "about_the_garden"
+  get "about_the_caravans", to: 'pages#about_the_caravan', as: "about_the_caravan"
+
   get "home", to: "pages#home", as: "home"
+
   get "inside", to: "pages#inside", as: "inside"
+
   get "contact_us/index"
   post "contact_us", :controller => "contact_us", :action => :create
+
   get "rooms", :controller => "rooms", :action => :index
+
   get "inquiries", :controller => "booking", :action => :index    
+
   post "inquiries", :controller => "booking", :action => :create    
+
   get "prices", :controller => "prices", :action => :index  
 
   devise_for :users
