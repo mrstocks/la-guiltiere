d0db3fe01a362afc56e185b32693fbb01d786671
Mrstocks <mrstocks@la-guiltiere.fr>
2014-02-14 14:32:03 +0100

    Finished the booking section

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,10 +1,26 @@
 class BookingController < ApplicationController
-  def index
-  end
+ 
+  public
+    def index
+    end
 
-  def create 
-     # Tell the UserMailer to send a welcome Email after save
-     BookingMailer.booking_email().deliver
-  end
-  
+    def create
+      #i hate this FIXME 
+      text = booking_text_params
+      date = booking_date_params
+      people = params[:people_on_board]
+
+      # Tell the UserMailer to send a welcome Email after save
+      BookingMailer.booking_email(text, date, people).deliver
+    end
+
+  private 
+    def booking_text_params
+      params.require(:booking).permit(:arrival, :departure, :animals, :email, :telephone)
+    end
+
+    def booking_date_params
+      params.require(:date).permit(:day_of_arrival, :month_of_arrival, :day_of_departure, :month_of_departure)
+    end
+
 end
--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,7 +1,13 @@
 class BookingMailer < ActionMailer::Base
   default from: "booking@la-guiltiere.fr"
 
-  def booking_email()
-    mail(to: "stocks.oliver@gmail.com", subject: 'booking email')
+  def booking_email(text, date, people)
+    # we have to do that so the view can access that variable
+    @text = text
+    @date = date
+    @people = people
+
+
+    mail(to: "stocks.oliver@gmail.com", subject: 'Booking form the website')
   end
 end
